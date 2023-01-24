b405704b5bed9f07b10a9ad2167e38e9c7968d24
tanerdundar <t.dundar145@mail.com>
2023-01-24 11:08:15 +0300

    simple starters

---
+++ photoGallery/script.js
@@ -0,0 +1,8 @@
+let photoOne = document.getElementById('firstBox')
+let photoTwo = document.getElementById('secondBox')
+let photoThree = document.getElementById('thirdBox')
+let photoFour = document.getElementById('fourthBox')
+
+photoOne.style.width=("50px")
+photoTwo.style.width=("50px")
+photoThree.style.width=("50px")
