1a0027fbd9c3c837b5015da1a64e9cb55313b900
tanerdundar <t.dundar145@mail.com>
2023-02-13 13:16:24 +0300

    a

--- newP/script.js
+++ newP/script.js
@@ -1,58 +1,58 @@
-let people= [
+let people = [
     {
-        name:"Mustafa Özbulut",
-        profession:"Web Developer",
-        description:"Kutu kola kadar.(Maaşallah)",
+        name: "Mustafa Özbulut",
+        profession: "Web Developer",
+        description: "Kutu kola kadar.(Maaşallah)",
         photo: "p/m.JPG"
     },
     {
-        name:"Sefa Çekerek",
-        profession:"None",
-        description:"Sürünüyor.",
+        name: "Sefa Çekerek",
+        profession: "None",
+        description: "Sürünüyor.",
         photo: "p/s.jpg"
     },
     {
-        name:"Erdoğan Evci",
-        profession:"Jigolo / Pornstar",
-        description:"33 santim diyolar ama ben hiç görmedim",
+        name: "Erdoğan Evci",
+        profession: "Jigolo / Pornstar",
+        description: "33 santim diyolar ama ben hiç görmedim",
         photo: "p/y.jpg"
     },
     {
-        name:"Ömer Faruk Aksak",
-        profession:"Memur",
-        description:"Takılıyor öyle...",
+        name: "Ömer Faruk Aksak",
+        profession: "Memur",
+        description: "Takılıyor öyle...",
         photo: "p/z.JPG"
     },
     {
-        name:"Ersin Evci",
-        profession:"#OpenToWork",
-        description:"Kim Busik (Kore'li bir devlet adamı ve yazardır.)",
+        name: "Ersin Evci",
+        profession: "#OpenToWork",
+        description: "Kim Busik (Kore'li bir devlet adamı ve yazardır.)",
         photo: "p/e.jpg"
     }
 ]
 
-let right =document.getElementById('inner-inside-3')
-let left =document.getElementById('inner-inside-1')
+let right = document.getElementById('inner-inside-3')
+let left = document.getElementById('inner-inside-1')
 const supriseButton = document.getElementById('surprise')
-let i= 2500;
+let i = 2500;
 getBio(i)
-left.addEventListener('click',()=> {
+left.addEventListener('click', () => {
     i--;
     getBio(i)
 })
-right.addEventListener('click',()=> {
+right.addEventListener('click', () => {
     i++;
     getBio(i)
 })
 
-supriseButton.addEventListener('click',()=> { 
-    getBio(Math.floor(Math.random()*15))
+supriseButton.addEventListener('click', () => {
+    getBio(Math.floor(Math.random() * 15))
 })
 
 function getBio(i) {
-    let k= i%5;
-        document.getElementById('img').style.backgroundImage = `url(${people[k].photo})`
-        document.getElementById('nam').innerHTML = people[k].name
-        document.getElementById('pro').innerHTML = people[k].profession
-        document.getElementById('bio').innerHTML = people[k].description
+    let k = i % 5;
+    document.getElementById('img').style.backgroundImage = `url(${people[k].photo})`
+    document.getElementById('nam').innerHTML = people[k].name
+    document.getElementById('pro').innerHTML = people[k].profession
+    document.getElementById('bio').innerHTML = people[k].description
 }
