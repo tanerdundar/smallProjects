5568fb05932bd8e124a4fcc8fa87417bc1cf015c
tanerdundar <t.dundar145@mail.com>
2023-02-28 00:38:38 +0300

    small updates

--- backgroundChanger/j.js
+++ backgroundChanger/j.js
@@ -1,14 +1,14 @@
-const key = document.getElementById('deneme');
- key.addEventListener('click', () => {
-    let color="#";
-    for (let i=0;i<6;i++) {
-       color += pickOne()  
-    }
-    document.getElementById('text').style.backgroundColor = color; 
-    document.getElementById('text2').innerHTML =color;
- })
- function pickOne() {
-    let f = Math.floor(Math.random()*16)
-    const arr = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-    return arr[f];
- }
+const key = document.getElementById('try');
+key.addEventListener('click', () => {
+   let color = "#";
+   for (let i = 0; i < 6; i++) {
+      color += pickOne()
+   }
+   document.getElementById('text').style.backgroundColor = color;
+   document.getElementById('text2').innerHTML = color;
+})
+function pickOne() {
+   let f = Math.floor(Math.random() * 16)
+   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+   return arr[f];
+}
